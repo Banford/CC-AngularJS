1142e3516d9d8cc9f6ad065a9a4965ee615821d8
Edward Spencer <efjspencer@gmail.com>
2014-09-28 15:44:18 +0100

    Step 1 commit

---
+++ CraftyMeetup/Angular/meetups.controller.js
@@ -0,0 +1,46 @@
+﻿(function () {
+    'use strict';
+    var meetups = [
+        {
+            name: "Crafty Coders",
+            location: "Basepoint",
+            email: "ian@craftycoders.com",
+            date: new Date("2014-10-02")
+        },
+        {
+            name: "Foo Coders",
+            location: "Brum",
+            email: "foo@foocoders.com",
+            date: new Date("2014-10-15")
+        },
+        {
+            name: "WooWeb",
+            location: "The Hive",
+            email: "wooweb@names.co",
+            date: new Date("2014-10-20")
+        }
+    ];
+
+    angular
+        .module('craftyMeetup')
+        .controller('meetupsController', function ($scope) {
+
+            $scope.submitNewMeetup = function (newMeetup) {
+                // ToDo: Implement
+            }
+
+            init();
+
+        function init() {
+            $scope.meetups = meetups;
+            $scope.newMeetup = {
+                name: "",
+                location: "",
+                email: "",
+                date: null
+            };
+        }
+    });
+
+    
+})();
---
+++ CraftyMeetup/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CraftyMeetup.MvcApplication" Language="C#" %>
